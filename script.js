931038acbc404547f1b7bc2b37bf4bb9a4a40709
AnonyMouse <84730158+carsondioxide@users.noreply.github.com>
2024-03-11 15:39:19 -0500

    Styling cleanup and improving scripting.

---
+++ script.js
@@ -0,0 +1,8 @@
+const star_signs = ['Aries','Taurus','Gemini','Cancer','Leo','Virgo','Libra','Scorpio','Sagittarius','Capricorn','Aquarius','Pisces'];
+
+// Keeps track of the user's star sign
+let user_sign = "";
+
+for(const star_sign in star_signs){
+    const sign = star_sign.toLowerCase();
+}
