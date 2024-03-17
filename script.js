c32c792613dfaec5b0a2e49cfe08cabac1c08883
AnonyMouse <84730158+carsondioxide@users.noreply.github.com>
2024-03-17 10:29:08 -0500

    Creates and displays random message.

--- script.js
+++ script.js
@@ -1,8 +1,60 @@
 const star_signs = ['Aries','Taurus','Gemini','Cancer','Leo','Virgo','Libra','Scorpio','Sagittarius','Capricorn','Aquarius','Pisces'];
 
+const controller = new AbortController();
+
 // Keeps track of the user's star sign
 let user_sign = "";
 
-for(const star_sign in star_signs){
-    const sign = star_sign.toLowerCase();
+function createRandomMessage(starSign){
+    let horoscope = document.getElementById("horoscope").innerHTML;
+
+    const luckyNum = Math.floor(Math.random() * 100);
+    const messageOptions = {
+        day: ['fortuitous', 'unlucky', 'fruitful', 'chaotic', 'unpredictable', 'challenging', 'rewarding'],
+        getDay(){
+            return this.day[Math.floor(Math.random() * this.day.length)];
+        },
+        improvement: ['improvements', 'no change', 'a decline'],
+        getImprovement(){
+            return this.improvement[Math.floor(Math.random() * this.improvement.length)];
+        },
+        aspect: ['romantic life', 'financial status', 'family life', 'sexual health', 'mental health', 'occupational environment'],
+        getAspect(){
+            return this.aspect[Math.floor(Math.random() * this.aspect.length)];
+        }
+    };
+
+    let h2 = document.createElement('h2');
+    h2.append(`${starSign}'s Daily Horoscope`);
+    let ul = document.createElement('ul');
+    let dailyMessage = document.createElement('li');
+    dailyMessage.append(`Daily Message:\t\tYou will have a ${messageOptions.getDay()} day today.`);
+    let prospects = document.createElement('li');
+    prospects.append(`Future Prospects:\t\tYou will see ${messageOptions.getImprovement()} in your ${messageOptions.getAspect()}`);
+    let luckyNumber = document.createElement('li');
+    luckyNumber.append(`Lucky Number:\t\tYour lucky number today is ${luckyNum}`);
+
+    ul.append(dailyMessage, prospects, luckyNumber);
+
+    document.getElementById('horoscope').append(h2, ul);
 }
+
+star_signs.forEach((Sign)=>{
+    let sign = Sign.toLowerCase();
+    let elem = document.getElementById(sign);
+    elem.addEventListener("click", () => {
+        user_sign = Sign;
+        console.log(Sign);
+        document.getElementById("centered-content").style.display = "none";
+        document.getElementById("horoscope").style.display = "block";
+        createRandomMessage(Sign);
+        controller.abort();
+    },
+    {signal: controller.signal});
+});
+
+
+// TO DO:   remove the current content from view on click
+//          create a random message for the user
+//          add the message to the section with id "horoscope"
+//          change the display of the section from none to something visible
